525dbe1011b43f50c06d26af32b8e50825e4df19
唐白宇 <1583673832@qq.com>
2021-07-19 13:44:46 +0800

    上传项目

---
+++ src/main/java/com/hartend/crm/HartendCrmApplication.java
@@ -0,0 +1,13 @@
+package com.hartend.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HartendCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HartendCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hartend/crm/HartendCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hartend.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HartendCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
